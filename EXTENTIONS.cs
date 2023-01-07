63e181024efe4040be92d6a44fd15824d66f1c58
Mohamed Ragab <mr2837465@gmail.com>
2023-01-07 12:24:13 +0200

    Init Work

--- EXTENTIONS.cs
+++ EXTENTIONS.cs
@@ -3,6 +3,6 @@
     public static class EXTENTIONS
     {
         public static List<T> AsList<T>(this IEnumerable<T> source) =>
-                 (source is null or List<T>) ? (List<T>)source : source.ToList();
+                 (source is null or List<T>) ? AsList(source) : source.ToList();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 using File_Transfer.Services;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
