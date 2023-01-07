102924a4424fd2fbcc64966ebf4475e719630ff0
Sharaf-Mansour <sharaf_1015@yahoo.com>
2023-01-07 13:10:46 +0200

    Fix AsList

--- EXTENTIONS.cs
+++ EXTENTIONS.cs
@@ -1,8 +1,6 @@
-﻿namespace File_Transfer
+﻿namespace File_Transfer;
+public static class EXTENTIONS
 {
-    public static class EXTENTIONS
-    {
-        public static List<T> AsList<T>(this IEnumerable<T> source) =>
-                 (source is null or List<T>) ? AsList(source) : source.ToList();
-    }
+    public static List<T> AsList<T>(this IEnumerable<T> source) =>
+             (source is null or List<T>) ? (List<T>)(source ?? Array.Empty<T>()) : source.ToList() ;
 }
